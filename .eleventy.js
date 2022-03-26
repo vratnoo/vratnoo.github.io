ded0593f8e5a3308d71fcf57620a673417a12012
vratnoo <vratnoo@gmail.com>
2022-03-26 22:07:17 +0530

    finished TR blog clone with lifes firsrt dark mode

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Return your Object options:
   eleventyConfig.addPassthroughCopy("./src/css/style.css");
+  eleventyConfig.addPassthroughCopy("./src/js/basic.js");
   eleventyConfig.addPassthroughCopy("./src/assets");
   // eleventyConfig.addWatchTarget("./src/scss/");
   return {
---
+++ src/me.md
@@ -0,0 +1,20 @@
+---
+layout: me.njk    
+title: about me
+
+---
+
+## Hi, I am vikram Ratnoo
+I am from rajasthan.
+
+## What I am doing now
+*Update Februray 24th, 2022*
+
+*vikram Ratnoo here*
+- Eleventy js
+- Lorem, ipsum dolor.
+- Ratnoo
+- Charan [vikram]('/#' "Wikipedia")
+
+### quotes
+> here we are 
