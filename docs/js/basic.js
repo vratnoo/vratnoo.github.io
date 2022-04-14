7831793630c24541fd2f5338718b4b1f6f833aed
vratnoo <vratnoo@gmail.com>
2022-04-14 19:51:48 +0530

    improved dark mode with persistant across page reload

--- docs/js/basic.js
+++ docs/js/basic.js
@@ -1,11 +1,33 @@
 console.log("oke this is working");
-// alert("hello world");
 
 var trigger = document.querySelector('#theme-switcher');
 var body = document.querySelector('body');
+// alert("hello world");
+// first get theme from local
+//  if theme = dark
+//  add dark class
+// else nothing
 
-trigger.addEventListener('click', function(event) {
+
+// if button clicked
+// toggle dark mode
+// toggle storage theme
+
+
+let theme = localStorage.getItem('theme')
+
+if(theme===undefined){
+    localStorage.setItem('theme','light')
+    theme = localStorage.getItem('theme')
+}
+// in case of next reload
+if(theme=='dark'){
+    body.classList.add(theme)
+}
     
-    body.classList.toggle('dark');
-
+// in case of click 
+trigger.addEventListener('click', function(event) {
+    body.classList.toggle('dark')
+    theme = (theme=='dark')?'light':'dark'
+    localStorage.setItem('theme',theme)
 });
